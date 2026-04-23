0ab0e827473db2f3b46f2fb163be97acb8228f1f
Eduardo Jaureguy <luisesantiago05@gmail.com>
2026-04-23 15:33:59 -0400

    feat(pessoas): Adiciona os métodos do CRUD para o PessoaService

--- src/app/services/pessoa.service.ts
+++ src/app/services/pessoa.service.ts
@@ -1,16 +1,40 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { PessoaResponse } from '../models/pessoa.model';
+import { Pessoa, PessoaResponse } from '../models/pessoa.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PessoaService {
-  private http = inject(HttpClient)
-  private readonly API = environment.apiUrl
+  // Injeta o serviço para realizar as requisições
+  private http = inject(HttpClient);
 
+  // Recupera do environment a URL da API
+  private readonly API = environment.apiUrl;
+
+  // Lista todos as pessoas cadastradas
   listar(offset: number = 0) {
-    return this.http.get<PessoaResponse>(`${this.API}?offset=${offset}`)
+    return this.http.get<PessoaResponse>(`${this.API}?offset=${offset}`);
+  }
+
+  // Retorna a pessoa com o CPF passado como parâmetro
+  buscarPorCpf(cpf: string) {
+    return this.http.get<Pessoa>(`${this.API}${cpf}`);
+  }
+
+  // Cria um registro novo
+  criar(pessoa: Pessoa) {
+    return this.http.post<Pessoa>(this.API, pessoa);
+  }
+
+  // Atualiza a pessoa com o CPF passado como parâmetro
+  atualizar(cpf: string, pessoa: Partial<Pessoa>) {
+    return this.http.put<Pessoa>(`${this.API}${cpf}`, pessoa);
+  }
+
+  // Exclui a pessoa com o CPF passado como parâmetro
+  excluir(cpf: string) {
+    return this.http.delete(`${this.API}${cpf}`);
   }
 }
